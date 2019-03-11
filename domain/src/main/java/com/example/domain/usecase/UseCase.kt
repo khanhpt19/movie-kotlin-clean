e679062e9f51143a2fa4834d1ed290e29d7d9046
pham.tien.khanh <FRAMGIA\pham.tien.khanh@framgia-0256-pc.framgia.com>
2019-03-11 08:17:02 +0700

    add domain

---
+++ domain/src/main/java/com/example/domain/usecase/UseCase.kt
@@ -0,0 +1,6 @@
+package com.example.domain.usecase
+
+abstract class UseCase<in Params, out T> where  T : Any {
+    abstract fun createObservable(params: Params? = null): T
+    abstract fun onCleared()
+}
---
+++ domain/src/main/java/com/example/domain/usecase/movie/GetMoviesUseCase.kt
@@ -0,0 +1,24 @@
+package com.example.domain.usecase.movie
+
+import com.example.domain.Constants
+import com.example.domain.model.Movie
+import com.example.domain.repository.MovieRepository
+import com.example.domain.usecase.UseCase
+import io.reactivex.Single
+
+open class GetMoviesUseCase constructor(
+    private val movieRepository: MovieRepository
+) : UseCase<GetMoviesUseCase.Params?, Single<List<Movie>>>() {
+    override fun createObservable(params: GetMoviesUseCase.Params?): Single<List<Movie>> {
+        params?.apply {
+            return movieRepository.getMoviesApi(params.type, params.fromServer)
+        }
+        return Single.error { Throwable(Constants.PARAMS_ERROR) }
+    }
+
+    override fun onCleared() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    data class Params(val type: String, val fromServer: Boolean)
+}
